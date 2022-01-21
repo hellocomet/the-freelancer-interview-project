283654d64622fb9c6b632d0c9990a239b498bd97
Simon Relet <simon.relet@gmail.com>
2022-01-21 11:52:30 +0100

    🔧 Update mock data

--- api/src/schema.ts
+++ api/src/schema.ts
@@ -1,11 +1,16 @@
 import { gql } from 'graphql-tag'
 import { Language, Resolvers } from '../generated/graphql'
 
+/**
+ * Use this object mock as if it was retrieved from a database.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const MOCK_FREELANCER = {
+  id: 'a6af7583-ff4b-44b5-abc7-daf5444f3b9b',
   avatar: {
-    '64x64': '',
-    '256x256': '',
-    '512x512': '',
+    '64x64': '/avatar/a6af7583-ff4b-44b5-abc7-daf5444f3b9b/64x64.png',
+    '256x256': '/avatar/a6af7583-ff4b-44b5-abc7-daf5444f3b9b/256x256.png',
+    '512x512': '/avatar/a6af7583-ff4b-44b5-abc7-daf5444f3b9b/512x512.png',
   },
   firstname: 'Gordon',
   lastname: 'Champlin',
